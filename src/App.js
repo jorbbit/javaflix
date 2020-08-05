2cd913284a6c05a55d41b9404fb412e80477e8f4
jorbbit <jonatas.sp@gmail.com>
2020-08-04 22:50:49 -0300

    Setup inicial do projeto

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Menu from './components/Menu';
+import BannerMain from './components/BannerMain';
+
+function App() {
+  return (
+    <div>
+      
+      <Menu />
+
+      <BannerMain  
+        videoTitle={}
+      />
+    </div>
+  );
+}
+
+export default App;
